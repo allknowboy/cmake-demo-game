32c7060d0fc88017bd81b93879a9e35481636961
包打听 <death_loong@foxmail.com>
2023-06-14 23:58:21 +0800

    加入鼠标键盘控制

---
+++ src/control/input.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <unordered_map>
+#include "glm/glm.hpp"
+
+struct vec2_ushort{
+    unsigned short x_;
+    unsigned short y_;
+};
+
+
+class Input {
+public:
+    /// 判断当前帧 键盘按键是否按下
+    static bool GetKey(unsigned short key_code);
+
+    /// 判断当前帧 键盘按键是否处于按下状态
+    static bool GetKeyDown(unsigned short key_code);
+
+    /// 判断当前帧 键盘按键是否按下并松开
+    static bool GetKeyUp(unsigned short key_code);
+
+    /// 记录按键事件，键盘按下记录数+1，键盘弹起记录数-1，当记录数为0，说明此时没有按键。
+    /// \param key_code
+    /// \param action 0松手 1按下 2持续按下
+    static void RecordKey(int key_code, unsigned char key_action);
+
+    /// 判断是否按了鼠标某个按钮
+    /// \param mouse_button_index   0 表示主按钮（通常为左按钮），1 表示副按钮，2 表示中间按钮。
+    /// \return
+    static bool GetMouseButton(unsigned short mouse_button_index);
+
+    /// 指定鼠标按键是否处于按下的状态
+    /// \param mouse_button_index   0 表示主按钮（通常为左按钮），1 表示副按钮，2 表示中间按钮。
+    /// \return
+    static bool GetMouseButtonDown(unsigned short mouse_button_index);
+
+    /// 鼠标按钮是否松开
+    /// \param mouse_button_index   0 表示主按钮（通常为左按钮），1 表示副按钮，2 表示中间按钮。
+    /// \return
+    static bool GetMouseButtonUp(unsigned short mouse_button_index);
+
+    static vec2_ushort mousePosition() { return mouse_position_; }
+    /// 设置鼠标位置
+    /// \param x
+    /// \param y
+    static void set_mousePosition(unsigned short x, unsigned short y){ mouse_position_.x_ = x; mouse_position_.y_ = y; }
+
+    static short mouse_scroll(){ return mouse_scroll_; }
+    /// 记录鼠标滚轮事件
+    /// \param mouse_scroll
+    static void RecordScroll(short mouse_scroll){ mouse_scroll_ += mouse_scroll; }
+
+    /// 刷帧
+    static void Update();
+private:
+    static std::unordered_map<unsigned short, unsigned char> key_event_map_; // 按键状态 0弹起 1按下 2持续按下
+    static vec2_ushort mouse_position_; // 鼠标位置
+    static short mouse_scroll_; // 鼠标滚轮值
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include<glm/gtc/matrix_transform.hpp>
-#include<glm/gtx/transform2.hpp>
-#include<glm/gtx/euler_angles.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform2.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/ext.hpp>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -22,7 +23,8 @@
 #include "component/component.h"
 #include "component/game_object.h"
 #include "component/transform.h"
-
+#include "control/input.h"
+#include "control/key_code.h"
 
 using namespace std;
 
@@ -32,13 +34,55 @@ static void error_callback(int error, const char* description)
     fprintf(stderr, "Error: %s\n", description);
 }
 
+/// 键盘回调
+/// \param window
+/// \param key 键盘按键编码
+/// \param scancode
+/// \param action 0松开 1按下 2一直按下
+/// \param mods
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    Input::RecordKey(key, action);
+}
+
+/// 鼠标按键回调
+/// \param window
+/// \param button
+/// \param action
+/// \param mods
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    Input::RecordKey(button, action);
+    std::cout << "mouse_button_callback:" << button << "," << action << std::endl;
+}
+
+/// 鼠标移动回调
+/// \param window
+/// \param x
+/// \param y
+void mouse_move_callback(GLFWwindow* window, double x, double y)
+{
+    Input::set_mousePosition(x,y);
+    std::cout << "mouse_move_callback:" << x << "," << y << std::endl;
+}
+
+/// 鼠标滚轮回调
+/// \param window
+/// \param x
+/// \param y
+void mouse_scroll_callback(GLFWwindow* window, double x, double y)
+{
+    Input::RecordScroll(y);
+    std::cout << "mouse_scroll_callback:" << x << "," << y << std::endl;
+}
+
 GLFWwindow* window;
 float m_time = 0.f;
 
 /// 初始化OpenGL
 void init_opengl()
 {
-    cout<<"init opengl"<<endl;
+    std::cout << "init opengl" <<endl;
 
     //设置错误回调
     glfwSetErrorCallback(error_callback);
@@ -52,7 +96,7 @@ void init_opengl()
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-    //创建窗口
+    // 创建窗口
     window = glfwCreateWindow(960, 640, "Simple example", NULL, NULL);
     if (!window)
     {
@@ -63,6 +107,12 @@ void init_opengl()
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
     glfwSwapInterval(1);
+
+    // 按键监听
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window,mouse_button_callback);
+    glfwSetScrollCallback(window,mouse_scroll_callback);
+    glfwSetCursorPosCallback(window,mouse_move_callback);
 }
 
 void processInput(GLFWwindow *window)
@@ -112,7 +162,10 @@ int main(int argc, char **argv)
     //第二个相机不能清除之前的颜色。不然用第一个相机矩阵渲染的物体就被清除 没了。
     camera_2->set_clear_flag(GL_DEPTH_BUFFER_BIT);
     camera_2->set_depth(1);
-    camera_2->set_culling_mask(0x01|0x02);
+    camera_2->set_culling_mask(0x02);
+
+    //上一帧的鼠标位置
+    vec2_ushort last_frame_mouse_position=Input::mousePosition();
 
     while (!glfwWindowShouldClose(window))
     {
@@ -131,13 +184,39 @@ int main(int argc, char **argv)
         camera_2->SetView(glm::vec3(0.1, 0, 0), glm::vec3(0, 1, 0));
         camera_2->SetProjection(60.f, ratio, 1.f, 1000.f);
 
-        //旋转物体
-        glm::vec3 rotation = transform->rotation();
-        rotation.y += 0.5f;
-        rotation.x -= 0.5f;
-        transform->set_rotation(rotation);
+
         transform->set_scale(glm::vec3(0.001f, 0.001f, 0.001f));
         transform->set_position(glm::vec3(0, 0.01, 0));
+
+        //旋转物体
+        if(Input::GetKeyDown(KEY_CODE_R)){
+            //旋转物体
+            glm::vec3 rotation = transform->rotation();
+            rotation.y += 0.5f;
+            rotation.x -= 0.5f;
+            transform->set_rotation(rotation);
+        }
+
+        //旋转相机
+        if(Input::GetKeyDown(KEY_CODE_LEFT_ALT) && Input::GetMouseButtonDown(MOUSE_BUTTON_LEFT)){
+            float degreesX = Input::mousePosition().x_ - last_frame_mouse_position.x_;
+            float degreesY = Input::mousePosition().y_ - last_frame_mouse_position.y_;
+            glm::mat4 old_mat4 = glm::mat4(1.0f);
+            std::cout << glm::to_string(old_mat4) << std::endl;
+            glm::mat4 rotate_mat4 = glm::rotate(old_mat4, glm::radians(degreesX * 100), glm::vec3(0.0f, 1.0f, 0.0f)); // 以相机所在坐标系位置，计算用于旋转的矩阵，这里是零点，所以直接用方阵。
+            rotate_mat4 = glm::rotate(rotate_mat4, glm::radians(degreesY * 100), glm::vec3(1.0f, 0.0f, 0.0f)); // 以相机所在坐标系位置，计算用于旋转的矩阵，这里是零点，所以直接用方阵。
+            glm::vec4 old_pos = glm::vec4(transform_camera_1->position(), 1.0f);
+            glm::vec4 new_pos = rotate_mat4 * old_pos; // 旋转矩阵 * 原来的坐标 = 相机以零点做旋转。
+            std::cout << glm::to_string(new_pos) << std::endl;
+            transform_camera_1->set_position(glm::vec3(new_pos));
+        }
+
+        last_frame_mouse_position = Input::mousePosition();
+        //鼠标滚轮控制相机远近
+        transform_camera_1->set_position(transform_camera_1->position()*=(10 - Input::mouse_scroll())/10.f);
+
+        //将状态为弹起的按键清理掉
+        Input::Update();
 
         //遍历所有相机，每个相机的View Projection，都用来做一次渲染。
         Camera::Foreach([&](){
